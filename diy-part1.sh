4d1c90171d4e9ae2fc658514aa1b4a6d921f4df2
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2023-11-23 07:24:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
-echo 'src-git libpcre2 https://github.com/openwrt/packages.git' >>feeds.conf.default
+
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
